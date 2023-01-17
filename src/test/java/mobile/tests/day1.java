6c6d6fb541bc4dfbc17e6163169236ea48d1433b
azatbayram <bayramov.azat.tkm@gmail.com>
2023-01-17 14:46:32 +0300

    open android calculator and test

--- src/test/java/mobile/tests/day1.java
+++ src/test/java/mobile/tests/day1.java
@@ -1,9 +1,12 @@
 package mobile.tests;
 
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileBy;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.remote.MobileCapabilityType;
+import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
@@ -24,6 +27,7 @@ public class day1 {
         desiredCapabilities.setCapability(MobileCapabilityType.VERSION, "8.0");
         //device name, if it is real device we need to pass UUID parameter
         desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel_2");
+        desiredCapabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
 
         //to open app
         //either you specify app apk file path
@@ -38,11 +42,73 @@ public class day1 {
         driver=new AppiumDriver<>(new URL("http://localhost:4723/wd/hub"),desiredCapabilities);
 
         Thread.sleep(3000);
+        MobileElement digit_2=driver.findElement(By.id("com.android.calculator2:id/digit_2"));
+        MobileElement plus=driver.findElement(MobileBy.AccessibilityId("plus"));
+        MobileElement equals=driver.findElement(MobileBy.AccessibilityId("equals"));
+        MobileElement result=driver.findElement(By.id("com.android.calculator2:id/result"));
+
+        digit_2.click();
+        plus.click();
+        digit_2.click();
+        equals.click();
+
+        String resultText= result.getText();
+        System.out.println(resultText);
+        Assert.assertEquals("4",resultText);
+
+        Thread.sleep(2000);
+
+        MobileElement digit_4=driver.findElement(By.id("com.android.calculator2:id/digit_4"));
+        MobileElement digit_5= driver.findElement(By.id("com.android.calculator2:id/digit_5"));
+        MobileElement multiply=driver.findElementByAccessibilityId("multiply");
+
+        digit_4.click();
+        multiply.click();
+        digit_5.click();
+        equals.click();
+        resultText=result.getText();
+        System.out.println(resultText);
+        Assert.assertEquals("20",resultText);
+
+        Thread.sleep(2000);
+
+        //25-15=10
+        MobileElement minus= driver.findElementByAccessibilityId("minus");
+        getDigit(2).click();
+        getDigit(5).click();
+        minus.click();
+        getDigit(1).click();
+        getDigit(5).click();
+        equals.click();
+        resultText=result.getText();
+        System.out.println(resultText);
+        Assert.assertEquals("10",resultText);
+
+
+
+
+        Thread.sleep(3000);
+
         //close app
         driver.closeApp();
 
 
+    }
 
+    public MobileElement getDigit(int digit){
+        String locatorText="com.android.calculator2:id/digit_"+digit;
+        return driver.findElement(By.id(locatorText));
+    }
+
+    @Test
+    public void test2(){
+        DesiredCapabilities desiredCapabilities=new DesiredCapabilities();
+        desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, Platform.ANDROID);
+        desiredCapabilities.setCapability(MobileCapabilityType.VERSION, "8.0");
+        desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel_2");
+        desiredCapabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
 
     }
+
+
 }
