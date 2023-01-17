4f03728615d76cb5cf5c4aca28d36b36f41cebeb
azatbayram <bayramov.azat.tkm@gmail.com>
2023-01-17 16:59:17 +0300

    open etsy app

--- src/test/java/mobile/tests/EtsyTest.java
+++ src/test/java/mobile/tests/EtsyTest.java
@@ -1,9 +1,12 @@
 package mobile.tests;
 
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileBy;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.remote.MobileCapabilityType;
+import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
@@ -24,8 +27,25 @@ public class EtsyTest {
         desiredCapabilities.setCapability(MobileCapabilityType.APP, System.getProperty("user.dir")+"//data/etsy.apk");
 
         driver=new AppiumDriver<>(new URL("http://localhost:4723/wd/hub"),desiredCapabilities);
-        Thread.sleep(5000);
+        Thread.sleep(3000);
+
+        MobileElement favorites= driver.findElementByAccessibilityId("Favorites tab, 2 of 5");
+        favorites.click();
+        Thread.sleep(3000);
+        MobileElement you= driver.findElement(MobileBy.AccessibilityId("You tab, 4 of 5"));
+        you.click();
+        Thread.sleep(3000);
+        MobileElement settings= driver.findElement(By.xpath("//*[@text='Settings']"));
+        settings.click();
+        Thread.sleep(3000);
+        MobileElement checkBox=driver.findElement(By.id("com.etsy.android:id/settings_checkbox"));
+        checkBox.click();
+        Thread.sleep(3000);
+        Assert.assertFalse(driver.findElement(By.id("com.etsy.android:id/settings_checkbox")).isSelected());
+
+        Thread.sleep(2000);
         driver.closeApp();
 
     }
+
 }
